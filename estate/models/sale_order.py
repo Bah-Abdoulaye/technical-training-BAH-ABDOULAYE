3ae8c8ac24a64bc78ea864378c5b157fa335ea97
Abdoulaye BAH <abbah@helb-prigogine.be>
2023-01-07 00:22:31 +0100

    ajout d'evenement au calendrier

--- estate/models/sale_order.py
+++ estate/models/sale_order.py
@@ -5,5 +5,19 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     
-
+    def add_envent_to_calendar(self):
+        # Appel de la méthode action_confirm parente
+        result = super().action_confirm()
+        # Pour chaque ligne de commande de la commande de vente
+        for line in self.order_line:
+            # Si un employé a été sélectionné
+            if line.employe_id:
+                # Création d'un événement dans le calendrier de l'employé
+                self.env['calendar.event'].create_event(
+                    line.employe_id.id,
+                    line.training_date,
+                    'Formation',
+                    'Formation prévue pour la date : %s' % line.training_date
+                )
+        return result
 
