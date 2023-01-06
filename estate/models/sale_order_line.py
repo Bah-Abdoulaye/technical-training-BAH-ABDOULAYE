27c5d2610e838ff8e460e1382cd36bb4ae2b36ce
Abdoulaye BAH <abbah@helb-prigogine.be>
2023-01-07 00:32:53 +0100

    ajout du type calendrier pour le champ training_day

--- estate/models/sale_order_line.py
+++ estate/models/sale_order_line.py
@@ -2,6 +2,6 @@ from odoo import fields, models, Command
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
-    training_date = fields.Char(string="Training Date")
+    training_date = fields.Datetime(string="Training Date")
     employe_id = fields.Many2one('res.partner', string='Employe')
     event_id = fields.Many2one('calendar.event', string='Event')
