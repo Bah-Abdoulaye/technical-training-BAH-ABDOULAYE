f342926d0ec9634ba9befe75cf445e81bb25799c
Abdoulaye BAH <abbah@helb-prigogine.be>
2023-01-06 22:45:17 +0100

    ajout du champ "employé"

--- estate/models/sale_order_line.py
+++ estate/models/sale_order_line.py
@@ -1,5 +1,10 @@
 from odoo import fields, models, Command
 
+class Employe(models.Model):
+    _inherit = 'res.partner'
+    sale_order_line_ids = fields.One2many('sale.order.line', 'employe_id', string='Sale Order Lines')
+
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
     training_date = fields.Char(string="Training Date")
+    employe_id = fields.Many2one('res.partner', string='Employe')
