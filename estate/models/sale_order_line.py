0144834c14ae83a0ef440b2ebaa4118327d61960
Abdoulaye BAH <abbah@helb-prigogine.be>
2023-01-06 23:25:09 +0100

    ajout evenement au calendrier

--- estate/models/sale_order_line.py
+++ estate/models/sale_order_line.py
@@ -8,3 +8,4 @@ class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
     training_date = fields.Char(string="Training Date")
     employe_id = fields.Many2one('res.partner', string='Employe')
+    event_id = fields.Many2one('calendar.event', string='Event')
