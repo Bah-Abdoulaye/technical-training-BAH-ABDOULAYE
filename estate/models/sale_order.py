2abd1cd4691da074850c0a3914522bb7740b8a07
Abdoulaye BAH <abbah@helb-prigogine.be>
2023-01-07 00:14:35 +0100

    suppression d'une methode

--- estate/models/sale_order.py
+++ estate/models/sale_order.py
@@ -1,22 +1,9 @@
-from odoo import api, models, _, fields
+from odoo import models, _, fields
 from odoo.exceptions import ValidationError
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     
-    def action_confirm(self):
-        res = super(SaleOrder, self).action_confirm()
-        for order in self:
-            for line in order.order_line:
-                if line.employee_id and line.training_date:
-                    event_vals = {
-                        'name': line.name,
-                        'start': line.training_date,
-                        'stop': line.training_date,
-                        'allday': True,
-                        'partner_ids': [(4, line.employee_id.id)],
-                    }
-                    event = self.env['calendar.event'].create(event_vals)
-                    line.event_id = event.id
-        return res
+
+
