9e8301aad4975f61f023aa77513e72cc492d9f72
Abdoulaye BAH <abbah@helb-prigogine.be>
2023-01-07 01:56:06 +0100

    update dans le fichier sale_order_line.py

--- estate/models/sale_order_line.py
+++ estate/models/sale_order_line.py
@@ -12,3 +12,22 @@ class SaleOrderLine(models.Model):
         ('approved', 'Approved'),
         ('refused', 'Refused')
     ], string="Approval State", default='pending')
+
+
+    def _compute_approval_level_required(self):
+        for order in self:
+            total_amount = sum(line.price_subtotal for line in order.order_line)
+            for group in self.env['res.groups'].search([], order='approval_amount desc'):
+                if total_amount <= group.approval_amount:
+                    order.approval_level_required = group.approval_level
+                    break
+
+#vérifie si l'utilisateur connecté a le niveau d'approbation requis pour approuver la commande
+    def approve_order(self):
+        user = self.env.user
+        if not user.has_group('sales_team.group_sale_manager'):
+            raise UserError(_("You must be a sales manager to approve orders."))
+        if user.approval_level < self.approval_level_required:
+            raise UserError(_("You don't have the required approval level to approve this order."))
+        self.approval_state = 'approved'
+        self.add_event_to_calendar()
