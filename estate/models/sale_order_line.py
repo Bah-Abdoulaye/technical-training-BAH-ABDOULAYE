bea3f0a981218655c26f54dd7f0f1d5ca5bacaf2
Abdoulaye BAH <abbah@helb-prigogine.be>
2023-01-07 01:43:29 +0100

    test

--- estate/models/sale_order_line.py
+++ estate/models/sale_order_line.py
@@ -5,3 +5,10 @@ class SaleOrderLine(models.Model):
     training_date = fields.Datetime(string="Training Date")
     employe_id = fields.Many2one('res.partner', string='Employe')
     event_id = fields.Many2one('calendar.event', string='Event')
+    approval_level_required = fields.Integer(string="Approval Level Required", compute='_compute_approval_level_required')
+    #permet de suivre l'état d'approbation de chaque commande (en attente d'approbation, approuvée ou refusée).
+    approval_state = fields.Selection([
+        ('pending', 'Pending'),
+        ('approved', 'Approved'),
+        ('refused', 'Refused')
+    ], string="Approval State", default='pending')
