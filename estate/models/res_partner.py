19d139c4c0179ac063c8be9385ffaca45f5a7e5a
Abdoulaye BAH <abbah@helb-prigogine.be>
2023-01-07 02:17:58 +0100

    update sale_order.py

--- estate/models/res_partner.py
+++ estate/models/res_partner.py
@@ -2,4 +2,15 @@ from odoo import fields, models
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
-    approval_level = fields.Integer(string='Approval Level')
+    #stocke le niveau d'approbation de chaque utilisateur
+    approval_level = fields.Integer(string="Approval Level", compute='_compute_approval_level')
+    max_amount = fields.Float(string="Maximum Allowed Order Amount")
+
+    def _compute_approval_level(self):
+        for partner in self:
+            if partner.user_ids:
+                user = partner.user_ids[0]
+                for group in user.groups_id:
+                    if group.approval_level:
+                        partner.approval_level = group.approval_level
+                        break
--- estate/models/sale_order.py
+++ estate/models/sale_order.py
@@ -4,10 +4,24 @@ from odoo.exceptions import ValidationError
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    
+
     def add_event_to_calendar(self):
         # Appel de la méthode action_confirm parente
         result = super().action_confirm()
+
+        user = self.env.user
+        # Calcul du montant total de la commande
+        total_amount = sum(line.price_subtotal for line in self.order_line)
+        # Vérifie si l'utilisateur est dans un groupe avec un seuil d'approbation suffisant
+        if not user.has_group('sales_team.group_manager_level_1') and total_amount >= 500:
+            raise UserError(_("You don't have sufficient privileges to confirm this order with a total amount greater than 500."))
+        if not user.has_group('sales_team.group_manager_level_2') and total_amount >= 2000:
+            raise UserError(_("You don't have sufficient privileges to confirm this order with a total amount greater than 2000."))
+        if not user.has_group('sales_team.group_manager_level_3') and total_amount >= 5000:
+            raise UserError(_("You don't have sufficient privileges to confirm this order with a total amount greater than 5000."))
+        # Vérifie si le partenaire a un montant maximum de commande autorisé et si la commande actuelle le dépasse
+        if self.partner_id.max_amount and total_amount > self.partner_id.max_amount:
+            raise UserError(_("This order exceeds the maximum allowed amount for the selected partner."))
         # Pour chaque ligne de commande de la commande de vente
         for line in self.order_line:
             # Si un employé a été sélectionné
@@ -20,4 +34,3 @@ class SaleOrder(models.Model):
                     'Formation prévue pour la date : %s' % line.training_date
                 )
         return result
-
